b8adb7eaf120e2a120b6cb0cf3e7c6faaca1f6f7
NoeWolf61872466 <125989325+NoeWolf61872466@users.noreply.github.com>
2023-03-13 08:15:04 +0200

    Create WordCounter.py

---
+++ WordCounter.py
@@ -0,0 +1,12 @@
+def count_words(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            contents = file.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {file_name} does not exist.")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {file_name} has about {num_words} words.")
+
+count_words('example.txt')
